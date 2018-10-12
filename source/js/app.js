bbdd970c6a3bd44696fb575af522dbf5b3ac6360
Sergey Shchukin <shchukinfish@gmail.com>
2018-10-13 02:38:30 +0300

    added scrollreveal
    Probably the last commit

--- source/js/app.js
+++ source/js/app.js
@@ -6,3 +6,57 @@ function showNav() {
     x.className = "responsive-nav";
   }
 }
+
+
+window.sr = ScrollReveal();
+sr.reveal('#header article.main-article', {
+  duration: 300
+});
+sr.reveal('#header img.img-phone', {
+  delay: 500,
+  duration: 300,
+  origin: 'bottom',
+  distance: '100px'
+});
+
+sr.reveal('#features .h2-rocks', {
+  delay: 300,
+  duration: 300,
+  origin: 'bottom',
+  distance: '100px'
+});
+
+sr.reveal('#features .car', {
+  delay: 400,
+  duration: 300,
+  origin: 'left',
+  distance: '100px'
+});
+
+sr.reveal('#features .shoe', {
+  delay: 600,
+  duration: 300,
+  origin: 'left',
+  distance: '100px'
+});
+
+sr.reveal('#features .device', {
+  delay: 800,
+  duration: 300,
+  origin: 'left',
+  distance: '100px'
+});
+
+sr.reveal('#discount .icon-twoPhones', {
+  delay: 600,
+  duration: 300,
+  origin: 'left',
+  distance: '200px'
+});
+
+sr.reveal('#discount .offer', {
+  delay: 600,
+  duration: 300,
+  origin: 'right',
+  distance: '200px'
+});
