342d5e5ae79bfed33ab19d4d31b0266fbba4be61
Sergey Shchukin <shchukinfish@gmail.com>
2018-10-13 02:05:16 +0300

    adapted nav-bar

--- source/js/app.js
+++ source/js/app.js
@@ -1,10 +1,8 @@
-
-// JavaScript
-window.sr = ScrollReveal(); 
-
-// sr.reveal('h1', {
-//     delay: 0,
-//     duration: 200,
-//     origin: 'bottom',
-//     distance: '100px' 
-// });
+function showNav() {
+  var x = document.getElementById("responsive-nav");
+  if(x.className === "responsive-nav") {
+    x.className += " unfold";
+  } else {
+    x.className = "responsive-nav";
+  }
+}
